61851c60871eb8f9808026ec934fb24b485e4eb0
tuctiki <16737971@qq.com>
2021-07-05 10:21:56 +0800

    add comments simplify comprefix

--- arrays/comprefix.py
+++ arrays/comprefix.py
@@ -1,22 +1,19 @@
+"""
+==========
+最长公共前缀
+==========
+编写一个函数来查找字符串数组中的最长公共前缀。
+如果不存在公共前缀，则返回''
+"""
+
+
 def basic(strs):
-    if len(strs) == 0:
-        return ''
-    elif len(strs) == 1:
-        return strs[0]
-    else:
-        done = False
-        result = ''
+    if not len(strs) == 0:
         i = 0
         for s in strs[0]:
-            j = 1
-            while j < len(strs):
-                if i < len(strs[j]) and strs[j][i] == s:
-                    j += 1
-                else:
-                    done = True
-                    break
-            if done:
-                break
-            result += s
+            for t in strs[1:]:
+                if not (i < len(t) and t[i] == s):
+                    return strs[0][:i]
             i += 1
-        return result
+        return strs[0][:i]
+    return ''
--- tests/arrays/test_comprefix.py
+++ tests/arrays/test_comprefix.py
@@ -4,7 +4,8 @@ from arrays import comprefix
 
 class Test(TestCase):
     def test_basic(self):
-        self.assertEqual("", comprefix.basic([]))
+        self.assertEqual('', comprefix.basic([]))
         self.assertEqual('test', comprefix.basic(['test']))
         self.assertEqual('test', comprefix.basic(['test', 'testb']))
         self.assertEqual('test', comprefix.basic(['testa', 'test', 'testxxb']))
+        self.assertEqual('', comprefix.basic(['testa', 'ast', 'testxxb']))
