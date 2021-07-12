5fee92045d73bc44a1d500463deb043e3b8a2fcb
tuctiki <16737971@qq.com>
2021-07-12 17:17:57 +0800

    sum of two numbers

---
+++ arrays/sum.py
@@ -0,0 +1,16 @@
+"""
+=======
+两数之和
+=======
+给定一个整数数组 nums 和一个目标值 target，请你在该
+"""
+def basic(nums, target):
+    dict0 = {}
+    i = 0
+    while i < len(nums):
+        if target - nums[i] in dict0.keys():
+            return [dict0.get(target - nums[i]), i]
+        else:
+            dict0[nums[i]] = i
+            i += 1
+    return []
