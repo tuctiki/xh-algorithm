885f21ad5b3d22c712e860a64f58e7d3a15d396c
taotao <16734971@qq.com>
2021-11-22 06:35:16 +0000

    rename to sum_two

--- tests/arrays/test_sum.py
+++
@@ -1,9 +0,0 @@
-from unittest import TestCase
-from arrays import sum
-
-
-class Test(TestCase):
-    def test_basic(self):
-        self.assertEqual([], sum.basic([], 1))
-        self.assertEqual([], sum.basic([2, 3], 1))
-        self.assertEqual([2, 3], sum.basic([1, 2, 3, 4, 9], 7))
